31cc3d01517763d7ba9b20c3ae274103372a9c91
rustybucket-cloud <83788852+rustybucket-cloud@users.noreply.github.com>
2021-07-24 19:21:39 -0600

    mr bird

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 (function() {
     const images = document.querySelectorAll('img');
     images.forEach( image => {
-        image.setAttribute('src','mr-bird.jpeg');
+        image.setAttribute('src','https://agitated-bassi-83ae31.netlify.app/mr-bird.jpeg');
     });
 })()
