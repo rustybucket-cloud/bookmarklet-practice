afd052b034e367c416a4d85dec5d003095de6c82
rustybucket-cloud <83788852+rustybucket-cloud@users.noreply.github.com>
2021-07-24 19:39:40 -0600

    js update

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,14 @@
 (function() {
     const images = document.querySelectorAll('img');
     images.forEach( image => {
-        image.setAttribute('src','https://agitated-bassi-83ae31.netlify.app/mr-bird.jpeg');
+        image.src.replace(image.src,'https://agitated-bassi-83ae31.netlify.app/mr-bird.jpeg');
+    });
+    const h1 = document.querySelectorAll('h1');
+    h1.forEach( header => {
+        header.textContent = 'Mr Bird';
+    });
+    const p = document.querySelectorAll('p');
+    p.forEach( para => {
+        para.innerHTML = 'Mr Bird';
     });
 })()
